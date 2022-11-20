9c03630e9f7bd99be2bb541714d72343b61c8c88
realtradam <github@tradam.dev>
2022-11-20 00:50:06 -0500

    drawing a triangle :D

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
+
+EXE := game
+BUILD_DIR := build
+INC_FLAGS := -Isrc -Iglfw/build/include -Iglad/include
+LINK_FLAGS := -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+
+
 default: src/main.cpp glad.o input.o
-	@mkdir -p build
-	g++ -o build/game src/main.cpp build/glad.o build/input.o -Isrc -Iglfw/build/include -Iglad/include glfw/build/lib/libglfw3.a -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+	@mkdir -p $(BUILD_DIR)
+	g++ -o $(BUILD_DIR)/$(EXE) src/main.cpp $(BUILD_DIR)/glad.o $(BUILD_DIR)/input.o $(INC_FLAGS) glfw/build/lib/libglfw3.a $(LINK_FLAGS)
 
 input.o: src/input.cpp src/input.h
-	@mkdir -p build
-	g++ -o build/input.o -c src/input.cpp -Isrc -Iglfw/build/include -Iglad/include -lGL -lX11 -lpthread -lXrandr -lXi -ldl
-
+	@mkdir -p $(BUILD_DIR)
+	g++ -o $(BUILD_DIR)/input.o -c src/input.cpp $(INC_FLAGS) $(LINK_FLAGS)
 glad.o: glad/src/glad.c
-	@mkdir -p build
-	g++ -o build/glad.o -c glad/src/glad.c -Isrc -Iglfw/build/include -Iglad/include -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+	@mkdir -p $(BUILD_DIR)
+	g++ -o $(BUILD_DIR)/glad.o -c glad/src/glad.c $(INC_FLAGS) $(LINK_FLAGS)
 
 .PHONY: run
 run: default
-	./build/game
+	$(BUILD_DIR)/$(EXE)
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,36 @@
 
 #include <iostream>
 
+const char *vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)";
+const char *fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+} 
+)";
+
+//float vertices[] = {
+//     0.5f,  0.5f, 0.0f,  // top right
+//     0.5f, -0.5f, 0.0f,  // bottom right
+//    -0.5f, -0.5f, 0.0f,  // bottom left
+//    -0.5f,  0.5f, 0.0f   // top left 
+//};
+//unsigned int indices[] = {  // note that we start from 0!
+//    0, 1, 3,   // first triangle
+//    1, 2, 3    // second triangle
+//};  
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -35,6 +65,74 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// Vertex Input
+	float vertices[] = {
+		-0.5f,  -0.5f, 0.0f,
+		0.5f,  -0.5f, 0.0f,
+		0.0f,   0.5f, 0.0f
+	};
+
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER); // create a blank shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	{ // check if shader compiled successfully
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if(!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	{ // check if shader compiled successfully
+		int success;
+		char infoLog[512];
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if(!success)
+		{
+			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	{ // check if shader compiled successfully
+		int success;
+		char infoLog[512];
+		glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+		if(!success)
+		{
+			glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+	}
+
+	glDeleteShader(vertexShader); // we dont need these anymore once they are linked in the shader program
+	glDeleteShader(fragmentShader);
+
+	unsigned int VBO, VAO;
+	glGenBuffers(1, &VBO); // generate the buffer
+	glGenVertexArrays(1, &VAO); // generate array
+	glBindVertexArray(VAO); // bind array to apply "settings" to it
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind(activate) the buffer to the ARRAY_BUFFER
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // upload the data
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // configure how the vertex array looks
+	glEnableVertexAttribArray(0);
+
+
 	// game loop
 	while(!glfwWindowShouldClose(window))
 	{
@@ -42,6 +140,11 @@ int main() {
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glUseProgram(shaderProgram); // this will activate the shader and use them for all subsequent shader and render calls
+		glBindVertexArray(VAO); // activate the preconfigured settings
+		glDrawArrays(GL_TRIANGLES, 0, 3); // :)
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
